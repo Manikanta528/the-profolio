f17ec689ce15211bb3fe879695edef3c116218dc
Manikanta528 <chmanikanta528@gmail.com>
2023-05-23 12:37:51 +0530

    packages & config added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode : 'class',
   content: [
   "./index.html",
   "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '420px',
+      'md': '640px',
+      'lg': '768px',
+      'xl': '1024px',
+      '2xl': '1280px',
+    },
+    extend: {
+      fontFamily : {
+        inter : ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary : '#4FD1C5',
+        backgroundDark : '#1A202C',
+        textPrimaryDark : '#EDF2F7',
+        textSecondaryDark : '#A0AEC0',
+        background: '#FFFFFF',
+        textPrimary : '#2D3748',
+        textSecondary : '#718096',
+      },
+      backgroundImage: {
+        'hero' : "url('./src/assets/main.svg')",
+      }
+
+    },
   },
   plugins: [],
 }
