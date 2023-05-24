43556517986bd88e343b5b3f8b46864351a07a64
Manikanta528 <chmanikanta528@gmail.com>
2023-05-24 08:05:15 +0530

    routing & theme change added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import { useState } from 'react';
+
 import LandingPage from "./pages/LandingPage";
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+
+
 
 function App() {
   const [theme, setTheme] = useState(true);
   const handleThemeChange = () => {
+    if(theme)
+      window.document.documentElement.classList.add('dark');
+    else
+      window.document.documentElement.classList.remove('dark');
     setTheme(!theme);
   }
   return (
-    <div className={theme&&"dark"||""}>
-      <LandingPage theme={theme} toggleTheme={handleThemeChange}/>
-    </div>
+    <BrowserRouter >
+      <Routes >
+        <Route path='/'  element={<LandingPage theme={theme} toggleTheme={handleThemeChange} />}/>
+        <Route path='/login'  element={<Login theme={theme} toggleTheme={handleThemeChange}  />}/>
+        <Route path='/profile' element={<Profile  theme={theme} toggleTheme={handleThemeChange} />}/>
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
