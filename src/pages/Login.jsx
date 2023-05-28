c6fe48c924fd46bb1875c2a3bd8897a8dfba2c7d
Manikanta528 <chmanikanta528@gmail.com>
2023-05-28 17:18:12 +0530

    new pages initial commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -28,7 +28,7 @@ function Login(props) {
     <div className='bg-hero h-screen bg-auto bg-no-repeat bg-center font-inter bg-background dark:bg-backgroundDark '>
     <Header toggleTheme={toggleTheme} theme={theme} flag={loginFlag} />
     <div className="h-[80%] flex flex-col items-center justify-center px-6 md:px-16 ">
-        <div className="bg-background dark:bg-textPrimary w-[300px] md:w-[500px]  rounded-3xl backdrop-blur flex flex-col items-center px-8 py-12 md:px-10 md:py-14 shadow">
+        <div className="bg-background dark:bg-textPrimary w-[300px] z-10 md:w-[500px]  rounded-3xl backdrop-blur flex flex-col items-center px-8 py-12 md:px-10 md:py-14 shadow">
             <h1 className="text-textPrimary dark:text-textPrimaryDark text-base md:text-2xl font-bold  ">Welcome to <span className="text-primary">The Profolio! 🚀</span> </h1>
             <p className="text-textSecondary dark:text-textSecondaryDark text-sm md:text-base text-center mt-4">Get Sign in to your account</p>
             <button onClick={handleGoogleAuth} className='flex gap-2 items-center border-2 px-4 py-2 mt-8 rounded text-textPrimary border-textPrimary dark:border-textPrimaryDark dark:text-textPrimaryDark'> <FcGoogle className='inline'/> <span className='hidden md:inline '>Sign in with</span> <span className=''> Google</span></button>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -30,8 +30,8 @@ function Profile(props) {
     getUser();
   }, []);
   return (
-    <div className="bg-hero bg-background dark:bg-backgroundDark text-textPrimary dark:text-textPrimaryDark">
-    <Header toggleTheme={toggleTheme} theme={theme} flag={flag}/>
+    <div className=" bg-background dark:bg-backgroundDark text-textPrimary dark:text-textPrimaryDark">
+    <Header toggleTheme={toggleTheme} theme={theme} flagSignIn={flag} user={true} currentPage={"Profile"}/>
     <div className=" px-6 md:px-16" >
       <main className="h-screen">
         <h1 className="mb-4">Github Authenticated Page</h1>
