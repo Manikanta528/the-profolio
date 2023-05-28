9e28bbedd9e6d494403e8e7458f88578664c6e0e
Manikanta528 <chmanikanta528@gmail.com>
2023-05-28 17:17:10 +0530

    more routes added

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import Login from './pages/Login';
 import Profile from './pages/Profile';
 
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import DiscoverPage from './pages/DiscoverPage';
+import FindPage from './pages/FindPage';
+import YourWorkPage from './pages/YourWorkPage';
 
 
 
 function App() {
   const [theme, setTheme] = useState(true);
+  
   const handleThemeChange = () => {
     if(theme)
       window.document.documentElement.classList.add('dark');
@@ -23,6 +27,9 @@ function App() {
         <Route path='/'  element={<LandingPage theme={theme} toggleTheme={handleThemeChange} />}/>
         <Route path='/login'  element={<Login theme={theme} toggleTheme={handleThemeChange}  />}/>
         <Route path='/profile' element={<Profile  theme={theme} toggleTheme={handleThemeChange} />}/>
+        <Route path='/Discover' element={<DiscoverPage  theme={theme} toggleTheme={handleThemeChange} />}/>
+        <Route path='/find' element={<FindPage  theme={theme} toggleTheme={handleThemeChange} />}/>
+        <Route path='/your-work' element={<YourWorkPage  theme={theme} toggleTheme={handleThemeChange} />}/>
       </Routes>
     </BrowserRouter>
 
