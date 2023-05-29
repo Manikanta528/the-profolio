5ddcc91293114d79f5ed0a30669a979b91ecd8e9
Manikanta528 <chmanikanta528@gmail.com>
2023-05-29 20:30:20 +0530

    form and profile added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,9 +2,11 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { account } from "../../utils";
+import { account , databases, storage } from "../../utils";
 
 import Header from "../components/Header";
+import { RxCross2 } from "react-icons/rx";
+
 
 function Profile(props) {
   const [user, setUser] = useState(null);
@@ -12,10 +14,14 @@ function Profile(props) {
   const flag = true;
   // eslint-disable-next-line react/prop-types
   const {toggleTheme,theme} = props
+
+  // get user auth details
   const getUser = async () => {
     const userData = (await account) && account.get();
     userData
-      .then((res) => setUser(res))
+      .then((res) => {
+        setUser(res)
+      })
       .catch((err) => {
         navigate("/");
         console.log(err);
@@ -29,24 +35,200 @@ function Profile(props) {
   useEffect(() => {
     getUser();
   }, []);
+
+
+  // Edit Profile toggle
+  const [profileEdit, setProfileEdit] = useState(false)
+  const handleEditProfile = () => {
+    setProfileEdit(!profileEdit)
+  }
+
+  // Profile Creation and updation of user data
+  const [profilePic, setProfilePic] = useState(null); // default profile pic 
+  const [name, setName] = useState("");
+  const [title, setTitle] = useState("");
+  const [bio, setBio] = useState("");
+  const [twitter, setTwitter] = useState("");
+  const [github, setGithub] = useState("");
+  const [inputSkill, setInputSkill] = useState('');
+  const [skills, setSkills] = useState([]);
+
+  const onChangeSkills = (e) => {
+    const { value } = e.target;
+    setInputSkill(value);
+  };
+
+  const handleSkills = (e) => {
+    const { key } = e;
+    const trimmedInput = inputSkill.trim();
+  
+    if (key === ',' && trimmedInput.length && !skills.includes(trimmedInput)) {
+      e.preventDefault();
+      setSkills(prevState => [...prevState, trimmedInput]);
+      setInputSkill('');
+    }
+  }
+
+  const handleSkillDelete = (skill) => {
+    const filteredSkills = skills.filter(item => skill != item);
+    setSkills(filteredSkills);
+  }
+
+  // updating profile in database and creating profile if not exists
+  const updateProfile = (response) => {
+    const promise =  databases.updateDocument(import.meta.env.VITE_DATABASE_ID , import.meta.env.VITE_USER_DETAILS_COLLECTION_ID, response.$id, {
+      name: name,
+      title: title,
+      bio: bio,
+      twitter: twitter,
+      github: github,
+      skills: skills
+    });
+
+    promise.then(function (response) {
+      console.log(response);
+      alert("Profile Updated");
+    }, function (error) {
+      console.log(error); // Failure
+    });
+    setProfileEdit(false);
+    
+  }
+    
+
+  
+
+  const createUserDocument =  (response)=> {
+    const promise =  databases.createDocument(import.meta.env.VITE_DATABASE_ID , import.meta.env.VITE_USER_DETAILS_COLLECTION_ID, response.$id, {
+      name: name,
+      title: title,
+      bio: bio,
+      twitter: twitter,
+      github: github,
+      skills: skills
+    });
+    promise.then(function (response) {
+      console.log(response);
+      alert("Profile Created");
+    }, function (error) {
+      updateProfile(response);
+      console.log(error); // Failure
+    }
+    );
+    setProfileEdit(false);
+  }
+
+  
+  
+
+  // handle profile submit
+  const handleProfile = (e) => { 
+    e.preventDefault()
+    //console.log(name,title,bio,twitter,github,skills);
+    const accountDetails = account.get();
+    accountDetails.then(function (response) {
+      createUserDocument(response);
+      setProfileEdit(false); 
+    }, function (error) {
+      console.log(error); // Failure  
+    });
+  }
+
+  useEffect(() => {
+    const userData = account.get();
+    userData.then(function (response) {
+        const promise = databases.getDocument(import.meta.env.VITE_DATABASE_ID , import.meta.env.VITE_USER_DETAILS_COLLECTION_ID, response.$id);
+        promise.then(function (response) {
+          //console.log(response);
+          setName(response.name);
+          setTitle(response.title);
+          setBio(response.bio);
+          setTwitter(response.twitter);
+          setGithub(response.github);
+          setSkills(response.skills);
+          setUserId(response.$id);
+        }, function (error) {
+          console.log(error); // Failure
+          setProfileEdit(true);
+        });
+        
+    }, function (error) {
+      console.log(error); // Failure 
+      setProfileEdit(true); 
+    });
+    
+  }, []);
+
+
   return (
-    <div className=" bg-background dark:bg-backgroundDark text-textPrimary dark:text-textPrimaryDark">
-    <Header toggleTheme={toggleTheme} theme={theme} flagSignIn={flag} user={true} currentPage={"Profile"}/>
+    <div className="dark:bg-hero bg-background dark:bg-backgroundDark text-textPrimary dark:text-textPrimaryDark">
+    <Header toggleTheme={toggleTheme} theme={theme} flagSignIn={!flag} user={true} currentPage={"Profile"}/>
     <div className=" px-6 md:px-16" >
       <main className="h-screen">
-        <h1 className="mb-4">Github Authenticated Page</h1>
-        {user && (
+
+        
+        {profileEdit && (<div>
+          <form onSubmit={handleProfile}>
+            <label htmlFor="profilePic">Profile Picture</label>
+            <input type='file' onChange={(e) => {setProfilePic(e.target.files[0]);}} />
+            <br />
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" placeholder="Full name" id="name" className="border-black border-2" value={name} onChange={(e) => setName(e.target.value)} required/>
+            <br />
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title" placeholder="dev" id="title" className="border-black border-2" value={title} onChange={(e) => setTitle(e.target.value)} required />
+            <br />
+            <label htmlFor="bio">Biography</label>
+            <textarea placeholder="bio" id="bio" className="border-black border-2" value={bio} onChange={(e) => setBio(e.target.value)} required/>
+            <br />
+            <label htmlFor="twitter">Twitter</label>
+            <input type="text" name="twitter" placeholder="user name" id="twitter" className="border-black border-2" value={twitter} onChange={(e) => setTwitter(e.target.value)} />
+            <br />
+            <label htmlFor="Github">Github</label>
+            <input type="text" name="Github" placeholder="user name" id="Github" className="border-black border-2" value={github} onChange={(e) => setGithub(e.target.value)} />
+            <br />
+            <div className="container">
+            <label htmlFor="skills">Skills</label>
+              {skills.map((skill,i) => <div key={i} className="inline" > <span>{skill}</span>
+              <RxCross2 onClick={()=>handleSkillDelete(skill)} className="inline"/>
+              </div>)}
+              <br />
+              <input
+                type="text"
+                id='skills'
+                value={inputSkill}
+                className="border-black border-2"
+                placeholder="Enter a skill and press comma to add it"
+                onKeyDown={handleSkills}
+                onChange={onChangeSkills}
+              />
+            </div>
+            <br />
+            <button type="submit" className="border-black border-2">save changes</button>
+          </form>
+        </div>)}
+        {user && !profileEdit && (
           <div>
-            <p>
-              User:{" "}
-              <span className="text-primary">{user.name}</span>
-            </p>
-            <p>
-              Email: <span className="text-primary">{user.email}</span>
-            </p>
-            <button onClick={logOut} className="">
-              Log Out
-            </button>
+          <div className="flex flex-col md:flex-row justify-center items-center md:items-start gap-4 md:gap-8 mt-8 md:mt-20">
+            <img src={ 'https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg'} alt="not Found" className="w-24 h-24 rounded-full md:w-32 md:h-32 shadow-xl"/>
+            <div className="flex flex-col items-center md:items-start  gap-3">
+              <h1 className="font-bold text-xl md:text-3xl text-textPrimary dark:text-textPrimaryDark">{name}</h1>
+              <h3 className="text-xs md:text-xl text-textSecondary dark:text-textSecondaryDark">{title}</h3>
+              <div className="flex gap-4">
+                {!profileEdit && <button className="text-textPrimary dark:text-textPrimaryDark bg-background dark:bg-backgroundDark border-2 px-4 py-2 md:px-2 md:py-1 rounded hover:bg-primary/50 hover:border-primary/50 text-xs md:text-base" onClick={handleEditProfile}>Edit Profile</button>}
+                <button className="border-2 hover:bg-red-500/50 px-4 py-2 rounded text-textPrimary dark:text-textPrimaryDark hover:border-red-500/50 text-xs md:px-2 md:py-1 md:text-base" onClick={logOut}>Log Out</button>
+              </div>
+            </div>
+          </div>
+          <hr className="my-12 border-textSecondary dark:border-textSecondaryDark "/>
+          <h1 className="font-bold text-xl md:text-2xl text-textPrimary dark:text-textPrimaryDark">Biography</h1>
+          <p className="text-textSecondary text-xs md:text-base dark:text-textSecondaryDark text-justify py-4">{bio}</p>
+          <h1 className="font-bold text-xl md:text-2xl mb-4 text-textPrimary dark:text-textPrimaryDark">Skills</h1>
+          <div className="flex flex-wrap gap-4 mb-4">
+          {skills.map((skill,i) => <div key={i} className="inline border-2 border-textPrimaryDark/50 rounded dark dark:border-primary " > <img src={'https://img.shields.io/badge/'+skill+'-FFFFFF?style=for-the-badge&logo='+skill} alt={skill} className="rounded-sm"/></div>)}
+          </div>
+          <h1 className="font-bold text-xl md:text-2xl mb-4 text-textPrimary dark:text-textPrimaryDark">Socials</h1>
+
           </div>
         )}
       </main>
