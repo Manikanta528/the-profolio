0d255a8b80c9e2da6af82ae42ddfa299e2dba427
Manikanta528 <chmanikanta528@gmail.com>
2023-05-30 16:39:35 +0530

    Edit profile ui enhancements

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -46,7 +46,8 @@ function Profile(props) {
   };
 
   // Profile Creation and updation of user data
-  const [profilePic, setProfilePic] = useState(null); // default profile pic
+  const [profilePic, setProfilePic] = useState([false,'']); 
+  const [img, setImg] = useState();
   const [name, setName] = useState("");
   const [title, setTitle] = useState("");
   const [bio, setBio] = useState("");
@@ -77,11 +78,12 @@ function Profile(props) {
   };
 
   // updating profile in database and creating profile if not exists
-  const updateProfile = (response) => {
+  const updateProfile = (id) => {
+
     const promise = databases.updateDocument(
       import.meta.env.VITE_DATABASE_ID,
       import.meta.env.VITE_USER_DETAILS_COLLECTION_ID,
-      response.$id,
+      id,
       {
         name: name,
         title: title,
@@ -92,12 +94,53 @@ function Profile(props) {
       }
     );
 
+    if (img) {
+      //console.log(img);
+      userProfilePicExists(id).then((exists) => {
+        if(exists){
+          const promise2 = storage.deleteFile(
+            import.meta.env.VITE_USER_PROFILE_BUCKET_ID,
+            id
+          );
+          promise2.then(
+            function (response) {
+              //console.log(response);
+              const promise3 = storage.createFile(
+                import.meta.env.VITE_USER_PROFILE_BUCKET_ID,
+                id,
+                img);
+                promise3.then(function (response) {
+                  setProfilePic([true, id]);
+                },function (error) {
+                  //console.log(error); // Failure
+                });
+            },
+            function (error) {
+              //console.log(error); // Failure
+            }
+          );
+        }else{
+          console.log(id);
+          const promise3 = storage.createFile(
+            import.meta.env.VITE_USER_PROFILE_BUCKET_ID,
+            id,
+            img);
+            promise3.then(function (response) {
+              setProfilePic([true, id]);
+              //console.log(id);
+            },function (error) {
+              //console.log(error); // Failure
+            });
+        }
+      });
+    }
+
     promise.then(
       function (response) {
         //console.log(response);
         //alert("Profile Updated");
         setPopUp(1);
-        console.log(popUp);
+        //console.log(popUp);
         setTimeout(() => {
           setPopUp(0);
         }, 5000);
@@ -106,14 +149,15 @@ function Profile(props) {
         console.log(error); // Failure
       }
     );
+
     setProfileEdit(false);
   };
 
-  const createUserDocument = (response) => {
+  const createUserDocument = (id) => {
     const promise = databases.createDocument(
       import.meta.env.VITE_DATABASE_ID,
       import.meta.env.VITE_USER_DETAILS_COLLECTION_ID,
-      response.$id,
+      id,
       {
         name: name,
         title: title,
@@ -123,6 +167,45 @@ function Profile(props) {
         skills: skills,
       }
     );
+    if (img) {
+      userProfilePicExists(id).then((exists) => {
+        if(exists){
+          const promise2 = storage.deleteFile(
+            import.meta.env.VITE_USER_PROFILE_BUCKET_ID,
+            id
+          );
+          promise2.then(
+            function (response) {
+              //console.log(response);
+              const promise3 = storage.createFile(
+                import.meta.env.VITE_USER_PROFILE_BUCKET_ID,
+                id,
+                img);
+              
+                promise3.then(function (response) {
+                  setProfilePic([true, id]);
+                },function (error) {
+                  //console.log(error); // Failure
+                });
+            },
+            function (error) {
+              //console.log(error); // Failure
+            }
+          );
+        }else{
+          const promise3 = storage.createFile(
+            import.meta.env.VITE_USER_PROFILE_BUCKET_ID,
+            id,
+            img);
+            promise3.then(function (response) {
+              setProfilePic([true, id]);
+            },function (error) {
+              //console.log(error); // Failure
+            });
+        }
+      });
+    }
+
     promise.then(
       function (response) {
         //console.log(response);
@@ -132,7 +215,6 @@ function Profile(props) {
         }, 5000);
       },
       function (error) {
-        
         //console.log(error); // Failure
       }
     );
@@ -147,10 +229,11 @@ function Profile(props) {
     accountDetails.then(
       function (response) {
         documentExists(response.$id).then((exists) => {
+          const id = response.$id;
           if (exists) {
-            updateProfile(response);
+            updateProfile(id);
           } else {
-            createUserDocument(response);
+            createUserDocument(id);
           }
         });
         setProfileEdit(false);
@@ -172,6 +255,24 @@ function Profile(props) {
         // console.log(response);
         // console.log(response.documents.filter(item => item.$id == documentId));
         if(response.documents.filter(item => item.$id == documentId).length > 0)
+        return true;
+        else  return false;
+      })
+      .catch((error) => {
+        console.error(error);
+        return false;
+      });
+  };
+
+  // check if user profile pic is existed or not
+  const userProfilePicExists = (documentId) => {
+    return storage
+      .listFiles(
+        import.meta.env.VITE_USER_PROFILE_BUCKET_ID
+      )
+      .then((response) => {
+        //console.log(response.files.filter(item => item.$id == documentId).length > 0);
+        if(response.files.filter(item => item.$id == documentId).length > 0)
         return true;
         else  return false;
       })
@@ -213,6 +314,13 @@ function Profile(props) {
             setProfileEdit(true);
           }
         });
+
+        userProfilePicExists(response.$id).then((exists) => {
+          if (exists) {
+            setProfilePic([true, response.$id]);
+            //console.log(response.$id);
+          }
+        });
       },
       function (error) {
         //console.log(error); // Failure
@@ -231,102 +339,123 @@ function Profile(props) {
         currentPage={"Profile"}
       />
       <div className=" px-6 md:px-16">
-        <main className="h-screen">
+        <main className="max-h-fit min-h-screen">
           {popUp === 1 && <PopUp msg="Successfully, Profile Updated" />}
           {popUp === 2 && <PopUp msg="Successfully, Profile Created" />}
           {profileEdit && (
-            <div>
-              <form onSubmit={handleProfile}>
-                <label htmlFor="profilePic">Profile Picture</label>
-                <input
-                  type="file"
-                  onChange={(e) => {
-                    setProfilePic(e.target.files[0]);
-                  }}
-                />
-                <br />
-                <label htmlFor="name">Name</label>
+            <div className="flex items-center flex-col">
+              <h1 className="font-bold text-xl lg:text-2xl mb-4 text-textPrimary dark:text-textPrimaryDark text-center mt-4">Edit Profile</h1>
+              <form onSubmit={handleProfile} className="mt-10 lg:w-3/5" >
+              <div className="flex flex-col  gap-4  mt-4">
+                <label htmlFor="name" className=" font-bold">Name <sup className="text-red-500">*</sup> </label>
                 <input
                   type="text"
                   name="name"
                   placeholder="Full name"
                   id="name"
-                  className="border-black border-2"
+                  className="border-2 flex-grow rounded py-1 px-2 hover:outline hover:outline-2 hover:outline-offset-0 hover:outline-primary/50 focus:outline focus:outline-2 focus:outline-offset-0 focus:outline-primary dark:bg-backgroundDark/50"
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                   required
                 />
-                <br />
-                <label htmlFor="title">Title</label>
+                </div>
+                <sup className="mb-4 text-[10px] pl-2 text-textSecondary dark:text-textSecondaryDark">40 characters allowed.</sup>
+
+                <div className="flex flex-col gap-4  mt-4">
+                <label htmlFor="title" className=" font-bold"><span className="w-24">Title </span><sup className="text-red-500">*</sup></label>
                 <input
                   type="text"
                   name="title"
-                  placeholder="dev"
+                  placeholder="like developer, designer, etc."
                   id="title"
-                  className="border-black border-2"
+                  className="border-2 flex-grow rounded py-1 px-2 hover:outline hover:outline-2 hover:outline-offset-0 hover:outline-primary/50 focus:outline focus:outline-2 focus:outline-offset-0 focus:outline-primary dark:bg-backgroundDark/50"
                   value={title}
                   onChange={(e) => setTitle(e.target.value)}
                   required
                 />
-                <br />
-                <label htmlFor="bio">Biography</label>
+                </div>
+                <sup className="mb-4 text-[10px] pl-2 text-textSecondary dark:text-textSecondaryDark">40 characters allowed.</sup>
+
+                <div className="flex flex-col  gap-4  ">
+                <label htmlFor="bio" className=" font-bold" > <span className="">Biography </span><sup className="text-red-500">*</sup></label>
                 <textarea
                   placeholder="bio"
                   id="bio"
-                  className="border-black border-2"
+                  className="border-2 flex-grow rounded py-1 px-2 hover:outline hover:outline-2 hover:outline-offset-0 hover:outline-primary/50 focus:outline focus:outline-2 focus:outline-offset-0 focus:outline-primary dark:bg-backgroundDark/50"
                   value={bio}
                   onChange={(e) => setBio(e.target.value)}
                   required
                 />
-                <br />
-                <label htmlFor="twitter">Twitter</label>
+                </div>
+                <sup className=" text-[10px] pl-2 text-textSecondary dark:text-textSecondaryDark">1024 characters allowed.</sup>
+
+                <div className="flex flex-col  gap-4 mt-4 ">
+                <label htmlFor="twitter" className=" font-bold ">Twitter <sup className="text-red-500">*</sup></label>
                 <input
                   type="text"
                   name="twitter"
-                  placeholder="user name"
+                  placeholder="twitters user name not url"
                   id="twitter"
-                  className="border-black border-2"
+                  className="border-2 flex-grow rounded py-1 px-2 hover:outline hover:outline-2 hover:outline-offset-0 hover:outline-primary/50 focus:outline focus:outline-2 focus:outline-offset-0 focus:outline-primary dark:bg-backgroundDark/50"
                   value={twitter}
                   onChange={(e) => setTwitter(e.target.value)}
                 />
-                <br />
-                <label htmlFor="Github">Github</label>
+                </div>
+                <sup className="mb-4 text-[10px] pl-2 text-textSecondary dark:text-textSecondaryDark">15 characters allowed.</sup>
+
+                <div className="flex flex-col gap-4 mt-4">
+                <label htmlFor="Github" className="font-bold">Github <sup className="text-red-500">*</sup></label>
                 <input
                   type="text"
                   name="Github"
                   placeholder="user name"
                   id="Github"
-                  className="border-black border-2"
+                  className="border-2 flex-grow rounded py-1 px-2 hover:outline hover:outline-2 hover:outline-offset-0 hover:outline-primary/50 focus:outline focus:outline-2 focus:outline-offset-0 focus:outline-primary dark:bg-backgroundDark/50"
                   value={github}
                   onChange={(e) => setGithub(e.target.value)}
                 />
-                <br />
-                <div className="container">
-                  <label htmlFor="skills">Skills</label>
-                  {skills.map((skill, i) => (
-                    <div key={i} className="inline">
-                      {" "}
-                      <span>{skill}</span>
-                      <RxCross2
-                        onClick={() => handleSkillDelete(skill)}
-                        className="inline"
-                      />
-                    </div>
-                  ))}
-                  <br />
+                </div>
+                <sup className="mb-4 text-[10px] pl-2 text-textSecondary dark:text-textSecondaryDark">39 characters allowed.</sup>
+                <div className="flex flex-col gap-4 mt-4">
+                  <label htmlFor="skills" className=" font-bold">Skills<sup className="text-red-500">*</sup></label>
                   <input
                     type="text"
                     id="skills"
                     value={inputSkill}
-                    className="border-black border-2"
+                    className="border-2 flex-grow rounded py-1 px-2 hover:outline hover:outline-2 hover:outline-offset-0 hover:outline-primary/50 focus:outline focus:outline-2 focus:outline-offset-0 focus:outline-primary dark:bg-backgroundDark/50"
                     placeholder="Enter a skill and press comma to add it"
                     onKeyDown={handleSkills}
                     onChange={onChangeSkills}
                   />
                 </div>
-                <br />
-                <button type="submit" className="border-black border-2">
-                  save changes
+                <sup className="mb-4 text-[10px] pl-2 text-textSecondary dark:text-textSecondaryDark">add minimum 3 skills.</sup>
+                <div className="flex gap-2 flex-wrap mb-4">
+                {skills.map((skill, i) => (
+                    <div key={i} className="px-2 py-1 border-2 w-fit border-textPrimaryDark rounded dark dark:border-primary hover:shadow-lg flex items-center gap-4">
+                      {" "}
+                      <span >{skill}</span>
+                      <RxCross2
+                        onClick={() => handleSkillDelete(skill)}
+                        className="hover:text-red-500 cursor-pointer"
+                      />
+                    </div>
+                  ))}
+                  </div>
+                  <div className="flex flex-col gap-4 ">
+                    <label htmlFor="profilePic" className=" font-bold">Profile Picture</label>
+                    <input
+                      type="file"
+                      className="border-2 flex-grow rounded py-1 px-2 hover:outline hover:outline-2 hover:outline-offset-0 hover:outline-primary/50 focus:outline focus:outline-2 focus:outline-offset-0 focus:outline-primary dark:bg-backgroundDark/50"
+                      onChange={(e) => {
+                        setImg(e.target.files[0]);
+                      }}
+                    />
+                    </div>
+                <sup className="mb-4 text-[10px] pl-2 text-textSecondary dark:text-textSecondaryDark">file upload might take 2-3 hrs to reflect in profile.</sup>
+          
+                <br/>
+                <button type="submit" className="text-textPrimaryDark my-4  border-none  font-bold px-6 py-3  rounded bg-primary hover:text-textPrimary hover:bg-primary/90 hover:border-primary/50 text-xs md:text-base hover:shadow-lg">
+                  Save Changes
                 </button>
               </form>
             </div>
@@ -335,7 +464,7 @@ function Profile(props) {
             <div>
               <div className="flex flex-col md:flex-row justify-center items-center md:items-start gap-4 md:gap-8  mb-12 mt-8 md:mt-20">
                 <img
-                  src={
+                  src={ profilePic[0] ? storage.getFilePreview(import.meta.env.VITE_USER_PROFILE_BUCKET_ID,profilePic[1]) :
                     "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
                   }
                   alt="not Found"
