0c8fc961a09a0a723f838cf1719e4f72cb6e7230
Manikanta528 <chmanikanta528@gmail.com>
2023-06-03 21:27:28 +0530

    ui enhancements

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -14,9 +14,9 @@ function LandingPage(props) {
   };
 
   return (
-    <div className="font-inter h-fit bg-background dark:bg-backgroundDark bg-hero bg-auto bg-no-repeat bg-center ">
+    <div className="font-inter h-fit bg-background dark:bg-backgroundDark bg-hero bg-auto bg-no-repeat bg-center max-h-fit min-h-screen">
       <Header toggleTheme={toggleTheme} theme={theme} flagSignIn={true}/>
-      <main className='h-screen px-6 md:px-16 flex flex-col justify-center items-center pb-24' >
+      <main className='mt-44 px-6 md:px-16 flex flex-col justify-center items-center pb-24' >
         <h1 className='text-3xl  select-none text-textPrimary sm:text-4xl w-full xl:w-4/5 lg:text-6xl font-bold text-center dark:text-textPrimaryDark ' data-aos="zoom-in" >Craft a Captivating Portfolio to Showcase <span className='text-primary'>Your Skills.</span></h1>
         <p className='text-xs pt-10 text-textSecondary sm:text-base w-full xl:w-3/5  text-center dark:text-textSecondaryDark' data-aos="zoom-in" >Connect. Collaborate. Create: Find User Projects to Elevate Your Portfolio&apos;s Impact. Engage with a vibrant community of creators, discover exciting projects that align with your interests, and amplify the impact of your portfolio by joining forces with like-minded individuals.</p>
         <div className='flex  flex-col gap-8 py-12 sm:flex-row sm:gap-12'>
