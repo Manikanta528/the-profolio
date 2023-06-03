f8b6b5812a6e5e4f4e90180f7aa3edbccd60508f
Manikanta528 <chmanikanta528@gmail.com>
2023-06-03 21:26:35 +0530

    ui enhancement

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,19 +1,22 @@
+/* eslint-disable react/prop-types */
 import {BsFillArrowUpRightSquareFill} from 'react-icons/bs'
 import { storage } from '../../utils';
+import { Link } from 'react-router-dom';
+import 'aos/dist/aos.css';
+
 
 function ProjectCard(props) {
     // eslint-disable-next-line react/prop-types
     const { projectName } = props.data;
 
   return (
-    <div className="group bg-cover dark:shadow-white/10 text-textPrimary dark:text-textPrimaryDark dark:border-textPrimaryDark text-base sm:text-lg w-full h-52 sm:w-64 sm:h-44 rounded-2xl  hover:shadow-2xl hover:bg-none cursor-pointer" >
-        <img src={storage.getFilePreview(import.meta.env.VITE_USER_PROJECTS_BUCKET_ID,props.data.$id)} alt=""  className='w-full h-full rounded-lg  border-textPrimary border-2 '/>
-        <div  className="relative bottom-14 flex flex-wrap items-center invisible justify-around group-hover:visible bg-backgroundDark dark:bg-background  m-2 rounded-md p-1 border-2">
-        <h3  className="w-40 truncate font-semibol text-textPrimaryDark dark:text-textPrimary">{projectName}</h3>
-        <BsFillArrowUpRightSquareFill className="bg-textPrimaryDark text-textPrimaryDark dark:bg-textPrimary dark:text-textPrimaryDark rounded group-hover:text-primary"/>
+    <Link  to="/project-overview" state={{data : props.data}} className="group bg-cover dark:shadow-white/10 text-textPrimary dark:text-textPrimaryDark dark:border-textPrimaryDark text-base sm:text-lg  w-full h-64 sm:w-96  rounded-2xl  hover:shadow-2xl  cursor-pointer"  data-aos="zoom-in">
+        <img src={storage.getFilePreview(import.meta.env.VITE_USER_PROJECTS_BUCKET_ID,props.data.$id)} alt=""  className='w-full h-full rounded-lg  '/>
+        <div className="relative bottom-14 flex  items-center  justify-around  rounded-b-md border-2 border-textPrimary dark:border-textSecondaryDark bg-background dark:bg-backgroundDark mt-2 p-3 sm:p-2">
+          <h3  className="w-40 truncate font-semibold text-textPrimary dark:text-textPrimaryDark">{projectName}</h3>
+          <BsFillArrowUpRightSquareFill className="bg-textPrimaryDark text-textPrimary dark:bg-textPrimary dark:text-textPrimaryDark rounded group-hover:text-primary"/>
         </div>
-        
-    </div>
+    </Link>
   )
 }
 
