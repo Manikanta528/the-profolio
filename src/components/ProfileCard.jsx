c7d2d611b2bd92e41d49a6ef60fb2e0d69967463
Manikanta528 <chmanikanta528@gmail.com>
2023-06-16 09:58:45 +0530

    ui enhancements

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -17,7 +17,7 @@ function ProfileCard(props) {
   }
 
   return (
-    <div className="max-h-fit w-[208px]  border-2 border-backgroundDark/10 dark:border-background/10 py-8 px-6 flex flex-col items-center rounded-md hover:shadow-2xl dark:shadow-background/10" data-aos="fade-up"  >
+    <div className="relative max-h-fit w-[208px]  border-[1px] border-backgroundDark/10 dark:border-background/10 py-8 px-6 flex flex-col items-center rounded-md hover:shadow-2xl  hover:border-white/0 hover:top-[-10px]" data-aos="fade-up"  >
       <img src={storage.getFilePreview(import.meta.env.VITE_USER_PROFILE_BUCKET_ID, id)} alt="" className="w-24 h-24 rounded-full" />
       <h1 className="font-bold mt-4 truncate w-full text-center">{name}</h1>
       <button className="bg-primary hover:bg-primary/70 w-full  text-textPrimaryDark hover:text-textPrimary font-semibold sm:font-bold py-2 px-4 mt-4 rounded" onClick={handleViewProfile}>View Profile</button>
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -13,9 +13,9 @@ function ProjectCard(props) {
     const { projectName } = props.data;
 
   return (
-    <Link  to="/project-overview" state={{data : props.data , findFlag : props.findFlag, discoverFlag : props.discoverFlag}} className="group bg-cover dark:shadow-white/10 text-textPrimary dark:text-textPrimaryDark dark:border-textPrimaryDark text-base sm:text-lg  w-full h-64 sm:w-96  rounded-2xl  hover:shadow-2xl  cursor-pointer" data-aos="fade-up" >
+    <Link  to="/project-overview" state={{data : props.data , findFlag : props.findFlag, discoverFlag : props.discoverFlag}} className="relative group bg-cover  text-textPrimary dark:text-textPrimaryDark dark:border-textPrimaryDark text-base sm:text-lg  w-full h-64 sm:w-96  rounded-2xl  shadow-xl hover:shadow-2xl hover:top-[-10px] cursor-pointer" data-aos="fade-up" >
         <img src={storage.getFilePreview(import.meta.env.VITE_USER_PROJECTS_BUCKET_ID,props.data.$id)} alt=""  className='w-full h-full rounded-lg  ' />
-        <div className="relative bottom-14 flex  items-center  justify-around  rounded-b-md border-2 border-t-white/0 dark:border-t-black/0 border-textPrimary dark:border-textSecondaryDark bg-background dark:bg-backgroundDark mt-2 p-3 sm:p-2">
+        <div className="relative bottom-12 flex  items-center  justify-around  rounded-b-md border-[1px] border-t-white/0 dark:border-t-black/0 border-textPrimary/0 dark:border-textSecondaryDark/0 bg-background dark:bg-textSecondaryDark mt-2 p-3 sm:p-2 group-hover:border-white/0">
           <h3  className="w-40 truncate font-semibold text-textPrimary dark:text-textPrimaryDark">{projectName}</h3>
           <BsFillArrowUpRightSquareFill className="bg-textPrimaryDark text-textPrimary dark:bg-textPrimary dark:text-textPrimaryDark rounded group-hover:text-primary"/>
         </div>
